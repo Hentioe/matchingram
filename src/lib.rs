7d458592f74f4474ab748b98f8c196e52a50d783
Hentioe <me@bluerain.io>
2020-09-01 02:13:41 +0000

    Add compile_rule function

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,17 @@ use models::Message;
 pub use rule::Rule;
 
 /// Match the message and the rule expression (that is, the string form of the rule).
-pub fn match_expression<S: Into<String>>(_message: &Message, _rule_expression: S) -> bool {
+pub fn match_expression<S: Into<String>>(_message: &Message, _expression: S) -> bool {
     panic!("This function has not been implemented yet!")
 }
 
 /// Match the message and the rule.
 pub fn match_rule(_message: &Message, _rule: &Rule) -> bool {
     panic!("This function has not been implemented yet!")
+}
+
+/// Compile a string expression into a rule.
+/// For details, please refer to [`Rule::new`](struct.Rule.html#method.new) function.
+pub fn compile_rule<S: Into<String>>(expression: S) -> Rule {
+    Rule::new(expression)
 }
--- src/rule.rs
+++ src/rule.rs
@@ -3,3 +3,12 @@
 /// Structured rule content.
 #[derive(Debug)]
 pub struct Rule {}
+
+impl Rule {
+    /// Use a string expression to create a rule object.
+    /// The string will be expanded into a specific structure,
+    /// and the rule object matching will have a faster speed because it does not need to be expanded again.
+    pub fn new<S: Into<String>>(_expression: S) -> Self {
+        Rule {}
+    }
+}

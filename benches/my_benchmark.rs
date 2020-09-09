93dcc3383e532ffd11f779cdf4295bcdd09e50cb
Hentioe <me@bluerain.io>
2020-09-09 18:47:10 +0800

    Add a test of 1mb rule text

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -9,6 +9,34 @@ fn rule_match(rule: &str) -> matchingram::result::Result<bool> {
     matchingram::rule_match(rule, &message)
 }
 
+fn compile_rule(rule: &str) -> matchingram::result::Result<matchingram::Matcher> {
+    matchingram::compile_rule(rule)
+}
+
+fn load_data_file(fname: &str) -> Vec<u8> {
+    use std::env;
+    use std::fs::read;
+
+    let mut fpath = env::current_dir().unwrap();
+
+    fpath.push("data");
+    fpath.push(fname);
+
+    match read(&fpath) {
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::NotFound {
+                eprintln!(
+                    "the data file `{}` failed to load, try to run `cargo make data.gen` command?",
+                    &fpath.to_str().unwrap()
+                );
+            }
+
+            panic!(e.to_string());
+        }
+        Ok(bytes) => bytes,
+    }
+}
+
 static MESSAGE_TEST: &'static str = r#"
 太平洋是地球上五大洋中面积最大的洋，面积1.813亿平方公里，它从北冰洋一直延伸至南冰洋，其西面为亚洲、大洋洲，东面为美洲，覆盖着地球约46%的水面及约32%的总面积，比地球上所有陆地面积的总和还要大。赤道将太平洋分为北太平洋及南太平洋。北面连接白令海峡，南面则以南纬60度为界。
 
@@ -41,10 +69,14 @@ fn criterion_benchmark(c: &mut Criterion) {
         message.text contains_all {"太" "平" "洋" "年" "月"}
     )"#;
 
+    let mb_rule_data = load_data_file("1mb-rule.txt");
+    let mb_rule = std::str::from_utf8(&mb_rule_data).unwrap();
+
     assert!(matches!(rule_match(regular_rule), Ok(true)));
     assert!(matches!(rule_match(regular_negate_rule), Ok(false)));
     assert!(matches!(rule_match(long_rule), Ok(true)));
     assert!(matches!(rule_match(longer_rule), Ok(true)));
+    assert!(matches!(compile_rule(mb_rule), Ok(_)));
 
     c.bench_function("rule_match regular-rule", |b| {
         b.iter(|| rule_match(black_box(regular_rule)))
@@ -57,6 +89,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
     c.bench_function("rule_match longer-rule", |b| {
         b.iter(|| rule_match(black_box(long_rule)))
+    });
+    c.bench_function("compile_rule 1mb-rule", |b| {
+        b.iter(|| compile_rule(black_box(mb_rule)))
     });
 }
 

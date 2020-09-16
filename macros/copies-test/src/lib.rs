192f6447ddcc8188fcd48d1e56f8267ea695d53c
Hentioe <me@bluerain.io>
2020-09-17 00:43:28 +0800

    Update macros design

--- macros/copies-test/src/lib.rs
+++ macros/copies-test/src/lib.rs
@@ -3,10 +3,10 @@ use copies::{def_it_works, AutoFrom};
 def_it_works!();
 
 #[derive(AutoFrom)]
-pub struct ModelA {
+pub struct ModelA<'a> {
     #[from(target = copies_test::ModelB)]
     #[from(target = copies_test::ModelC, rename = "_id")]
-    pub id: i64,
+    pub id: &'a i64,
 }
 
 pub struct ModelB {
@@ -15,4 +15,10 @@ pub struct ModelB {
 
 pub struct ModelC {
     pub _id: i64,
+}
+
+impl<'a> From<&'a ModelB> for ModelA<'a> {
+    fn from(b: &'a ModelB) -> Self {
+        ModelA { id: &b.id }
+    }
 }

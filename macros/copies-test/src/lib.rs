6d8e040278f8a92739b245f03c197f0387e7e1f7
Hentioe <me@bluerain.io>
2020-09-16 15:51:39 +0800

    Basic derive macros design

--- macros/copies-test/src/lib.rs
+++ macros/copies-test/src/lib.rs
@@ -1,3 +1,18 @@
-use copies::def_it_works;
+use copies::{def_it_works, AutoFrom};
 
 def_it_works!();
+
+#[derive(AutoFrom)]
+pub struct ModelA {
+    #[from(target = copies_test::ModelB)]
+    #[from(target = copies_test::ModelC, rename = "_id")]
+    pub id: i64,
+}
+
+pub struct ModelB {
+    pub id: i64,
+}
+
+pub struct ModelC {
+    pub _id: i64,
+}
--- macros/copies/src/lib.rs
+++ macros/copies/src/lib.rs
@@ -4,3 +4,8 @@ use proc_macro::TokenStream;
 pub fn def_it_works(_item: TokenStream) -> TokenStream {
     r#"pub fn it_works() -> bool { true }"#.parse().unwrap()
 }
+
+#[proc_macro_derive(AutoFrom, attributes(from))]
+pub fn derive_rename_attr(_item: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
